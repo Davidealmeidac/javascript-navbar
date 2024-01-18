8cf80c7a59b4de6a6fe09b158b78d5104e538e3d
Davi de Almeida <davidealmeidacastro@gmail.com>
2024-01-18 19:39:42 -0300

    Documento Formatado

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-let menuicon = document.querySelector('div.menu')
-    let menuhidden = document.querySelector('div#menu-hidden')
+let menuicon = document.querySelector("div.menu");
+let menuhidden = document.querySelector("div#menu-hidden");
 
-    menuicon.addEventListener('click', menu)
+menuicon.addEventListener("click", menu);
 
 function menu() {
-    if (menuhidden.style.display == 'none') {
-        menuhidden.style.display = 'flex'
-    } else {
-        menuhidden.style.display = 'none'
-    }
+  if (menuhidden.style.display == "none") {
+    menuhidden.style.display = "flex";
+  } else {
+    menuhidden.style.display = "none";
+  }
 }
