4a4e762e70980b7e1fb96e96a2ab74b26cb24935
Davi de Almeida <davidealmeidacastro@gmail.com>
2024-01-19 17:01:45 -0300

    Modo de exibição para medias maiores que 900px ok
    Foi refeito o header e adicionado uma imagem, faltam finalizar o posicionamento do reader e rever o css

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 let menuicon = document.querySelector("div.menu");
 let menuhidden = document.querySelector("div#menu-hidden");
+let menubicon = document.querySelector('li.product')
 
 menuicon.addEventListener("click", menu);
+menubicon.addEventListener("click", menu);
 
 function menu() {
   if (menuhidden.style.display == "none") {
