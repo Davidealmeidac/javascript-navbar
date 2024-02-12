3e174b8f96d7771e7dec1aa611c40179949471d6
Davi de Almeida <davidealmeidacastro@gmail.com>
2024-02-12 20:29:14 -0300

    toggle menu concluido npara mobile
    O menu toggle foi finalizado e o resultado  embora simples foi satisfatório. Resta realizar os medias queries.

---
+++ script.js
@@ -0,0 +1,13 @@
+let icon = document.getElementById('menu')
+let menu = document.getElementById('toggle-nav')
+let close = document.getElementById('close-page')
+
+function toggle() {
+
+    menu.style.display = 'flex'
+}
+
+function fechar() {
+
+    menu.style.display = 'none'
+}
